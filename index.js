9b9a8b706976d659aec952fe9da88c7390fefe65
Linus Kang <linus@kang.id.au>
2025-12-28 22:37:08 +0800

    Add additional log statement to index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 console.log('Hello from demo-deployment!');
 console.log('Application running successfully.');
 
+console.log('anither one')
+
 module.exports = {
   greet: (name) => `Hello, ${name}!`
 };
